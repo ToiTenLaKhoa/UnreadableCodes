5bb5c26bdf46fee70bb354f9c322e8264d299c17
ToiTenLaKhoa <anhkhoa.vv@gmail.com>
2023-03-30 09:29:27 +0700

    updated HASH checking

--- GEN.cpp
+++ GEN.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
+#include <windows.h>
 using namespace std;
 mt19937 rd(chrono::steady_clock::now().time_since_epoch().count());
+string Hashcode = "";
+string s;
 
 #define rand rd
 long long Rand(long long l, long long h)
@@ -8,11 +11,6 @@ long long Rand(long long l, long long h)
 	assert(l <= h);
 	return abs(l + rd() * 1LL * rd() % (h - l + 1));
 }
-
-
-// use any string in permutation.log for Hashcode
-const string Hashcode = "";
-string s;
 
 void trim(string &s)
 {
@@ -34,28 +32,39 @@ int ren(string name1, string name2)
 	return (rename(str1, str2) != 0);
 }
 
-void die(string msg)
+void msg(string msg)
 {
 	cout << msg << endl;
+}
+
+void die(string message)
+{
+	msg(message);
 	system("pause");
 	exit(0);
 }
 
-int main()
+bool hashcheck()
 {
-	if (!Hashcode.length())
-		die("Invalid Hashcode");
+	if (Hashcode.length() < 56)
+		return 0;
+	return 1;
+}
 
-	cout << "enter your file name > ";
-	string name;
-	cin >> name;
+void ps(int secs)
+{
+	Sleep(secs * 1000);
+}
 
-	if (ren(name, "old_" + name))
-		die("File not found");
+void cs()
+{
+	system("cls");
+}
 
+void cppexe(string name)
+{
 	ifstream ifile("old_" + name);
 	ofstream ofile(name);
-
 	int cnt = 1;
 	while (!ifile.eof())
 	{
@@ -80,5 +89,36 @@ int main()
 
 	ifile.close();
 	ofile.close();
+}
+
+int main()
+{
+	cout << "Loading HASH..." << endl;
+	ifstream hashin("HASH");
+	ps(1);
+	cs();
+	if (hashin.fail())
+		die("HASH not found.");
+	getline(hashin, Hashcode);
+	cout << "Checking Hashcode..." << endl;
+	ps(1);
+	cs();
+	if (!hashcheck())
+		die("Invalid HASH.");
+	string name;
+	bool valid = 0;
+	while (!valid)
+	{
+		valid = 1;
+		cout << "Enter your file name > ";
+		cin >> name;
+		if (ren(name, "old_" + name))
+		{
+			msg("File unreadable");
+			valid = 0;
+		}
+	}
+	cs();
+	cppexe(name);
 	return 0;
 }
